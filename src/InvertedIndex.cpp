934772920d40d5a983806f6959d9f73b1c1b4d51
Zayts <106050kcin@mail.ru>
2025-01-20 23:03:53 +0500

    rework#2

--- src/InvertedIndex.cpp
+++ src/InvertedIndex.cpp
@@ -3,30 +3,48 @@
 std::mutex mutex;
 
 void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs) {
-    docs.erase(docs.begin(), docs.end());
-    freq_dictionary.erase(freq_dictionary.begin(), freq_dictionary.end());
+    docs.clear();
+    freq_dictionary.clear();
     std::vector<std::thread> invertedIndexThread;
     for (size_t i = 0; i < input_docs.size(); ++i) {
         invertedIndexThread.emplace_back(
                 [this, doc = input_docs[i], doc_id = i]() {
+                    std::map<std::string, std::vector<Entry>> local_freq_dict;
                     std::stringstream streamDocs;
                     streamDocs << doc;
                     std::string tmp;
                     while (streamDocs >> tmp) {
                         bool match = false;
-                        std::lock_guard<std::mutex> lock(mutex);
-                        freq_dictionary[tmp];
-                        for (auto &i: freq_dictionary[tmp]) {
-                            if (i.doc_id == doc_id) {
-                                i.count++;
-                                match = true;
-                                break;
+                        if (local_freq_dict.find(tmp) != local_freq_dict.end()) {
+                            for (auto &entry: local_freq_dict[tmp]) {
+                                if (entry.doc_id == doc_id) {
+                                    entry.count++;
+                                    match = true;
+                                    break;
+                                }
                             }
                         }
                         if (!match) {
-                            freq_dictionary[tmp].push_back({doc_id, 1});
+                            local_freq_dict[tmp].push_back({doc_id, 1});
                         }
                     }
+                    std::lock_guard<std::mutex> lock(mutex);
+                    for (auto &[word, entries]: local_freq_dict) {
+                        for (auto &entry: entries) {
+                            bool found = false;
+                            for (auto &global_entry: freq_dictionary[word]) {
+                                if (global_entry.doc_id == entry.doc_id) {
+                                    global_entry.count += entry.count;
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if (!found) {
+                                freq_dictionary[word].push_back(entry);
+                            }
+                        }
+                    }
+
                 });
     }
     for (auto &thread: invertedIndexThread) {
